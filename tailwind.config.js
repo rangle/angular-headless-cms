63523e7a87a9d3f24fa5a6cdc419855e54010661
Bertrand <bertrandk@outlook.com>
2023-06-21 06:24:42 -0400

    Customize tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{html,ts,md}'],
+  content: ["./index.html", "./src/**/*.{html,ts,md}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: "Gilda Display",
+      secondary: "Barlow",
+      tertiary: "Barlow Condensed",
+    },
+    container: {
+      padding: {
+        DEFAULT: "15px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1140px",
+    },
+    extend: {
+      colors: {
+        primary: "#0a0a0a",
+        accent: {
+          DEFAULT: "#a37d4c",
+          hover: "#967142",
+        },
+      },
+    },
   },
   plugins: [],
 };
