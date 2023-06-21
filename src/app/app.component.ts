58c3a2c7cbf5ad0d0578e2e4e52facf357b12fc6
Bertrand <bertrandk@outlook.com>
2023-06-21 07:37:10 -0400

    Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
+    <app-footer></app-footer>
   `,
 })
 export class AppComponent {}
