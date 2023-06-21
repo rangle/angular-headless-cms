710efd362b65a0d3174b076394cced65db41c79e
Bertrand <bertrandk@outlook.com>
2023-06-21 08:26:15 -0400

    Add hero slider

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSliderComponent } from '../components/hero-slider/hero-slider.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  template: ` <div></div> `,
+  imports: [HeroSliderComponent],
+  template: `<app-hero-slider></app-hero-slider>`,
 })
 export default class HomeComponent {}
